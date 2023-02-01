a245a9a1aefa41b60b867c589ae83200b75bd4f2
Davner Firmino Cavalcante <davner23@gmail.com>
2023-02-01 10:50:33 -0400

    💽 Install: Instalação de arquivos

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-7xl font-bold text-center">Olá mundo =D!</h1>
+    </div>
+  );
+}
+
+export default App;
